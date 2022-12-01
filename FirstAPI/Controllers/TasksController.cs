9e2ef088a4f0f645ff7ba769a834397ee9cfd14a
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2022-12-01 22:36:15 +0530

    http get request created

---
+++ FirstAPI/Controllers/TasksController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Tasks()
+        {
+            var tasks = new string[] { "Task1", "Task2", "Task3" };
+            return Ok(tasks) ;
+        }
+
+        [HttpPost]
+        public IActionResult NewTask()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTask()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTask()
+        {
+            return Ok();
+        }
+    }
+}
